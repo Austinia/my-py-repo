eb1b3734c65cfad43a2c1deeb2d807d88f40bbd8
Austin Kim <kimdy90919@gmail.com>
2020-01-07 19:05:59 +0900

    2020-01-07 딕셔너리

--- Ed.py
+++ Ed.py
@@ -19,3 +19,18 @@ print(len(a)) #len은 문자열의 갯수나 리스트의 갯수를 알려주는
 print(str(a[1]) + "020\n") #str는 정수나 실수를 문자열로 바꿔주는 함수
 
 print("a = [1, 2, 3]\ndel a[1]\na\n[1, 3]\n")
+
+"""
+t1 = (1, 2, 'a', 'b')
+del t1[0]는 실행 되지 않는다, 튜플형은 리스트형과 달리 요소값을 변경할 수 없다:
+"""
+
+t1 = (1, 2, 'a', 'b')
+print(t1[0])
+print(t1[3])
+print(t1[1:])
+
+dic = {'name':'pey', 'phone':'0119993323', 'birth':'1119'}
+dic['job'] = 'janiter'
+print(dic)
+
